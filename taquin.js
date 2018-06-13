9658eda587e76ec82062ada3c591c722964726e2
VeroniqueRo <veronique.rouault@campus-numerique-in-the-alps.com>
2018-06-13 14:44:05 +0200

    Finalisation de la fonction ajout + modif erreur case

--- taquin.js
+++ taquin.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
 // Déclaration des Variables
+    let taillePlateau = 4;
 // Tableau des chiffres du taquin
     let plateau = [
         [1, 2, 3, 4],
@@ -9,24 +10,16 @@ $(document).ready(function () {
         [13, 14, 15, " "]
     ];
 
-// Exécution des fonctions
-// createPlateau();
-    chercheCaseVide(plateau);
-
 //*************************************************************//
 //               JQuery - Manipulation du DOM
 // *************************************************************//
 
-// Crée le plateau de jeu initial
-
-
-
     $('#initial').on('click',function () {
         $('#initial').off('click');
         for (let i = 0; i < plateau.length; i++) {
-            for (let j = 0; j < plateau[i].length; j++) {
-                $(".row" + i + " .case" + j).append(plateau[i][j]);
-                $('.row' + i + ' .case' + j).click(function () {
+            for (let j = 0; j < plateau.length; j++) {
+                $(".row" + i + " .cas" + j).append(plateau[i][j]);
+                $('.row' + i + ' .cas' + j).click(function () {
                     permute(i,j);
                 })
             }
@@ -37,11 +30,10 @@ $(document).ready(function () {
 //                    JavaScript - Factory
 //*************************************************************//
 
-    function draw() {
+    function redessinePlateau() {
         for (let i = 0; i < plateau.length; i++) {
             for (let j = 0; j < plateau.length; j++) {
-                $('.row' + i + ' .cas' + j).html( plateau[i][j]);
-
+                $('.row' + i + ' .cas' + j).html(plateau[i][j]);
             }
         }
     };
@@ -49,8 +41,7 @@ $(document).ready(function () {
     // Cherche quelle est la table vide
     function chercheCaseVide () {
         for (let i = 0; i < plateau.length; i++) {
-            for (let j = 0; j < plateau[i].length; j++) {
-
+            for (let j = 0; j < plateau.length; j++) {
                 if (plateau[i][j] === " ") {
                     return {"i":i,"j":j};
                 }
@@ -61,8 +52,7 @@ $(document).ready(function () {
     // Teste si la cellule est vide
     function celluleEstVide(i,j) {
 
-        if (plateau[i][j] === " ") {
-            $("this").addClass("celluleVide");
+        if (celluleExiste(i,j) && plateau[i][j] === " ") {
             return true;
         }
         return false;
@@ -71,7 +61,7 @@ $(document).ready(function () {
     // Teste si la cellule existe
     function celluleExiste(i,j) {
 
-        if ((i > 0 && i <= plateau.length) && (j > 0 && j <= plateau.length)) {
+        if ((i >= 0 && i < plateau.length) && (j >= 0 && j < plateau.length)) {
             return true;
         }
         return false;
@@ -80,10 +70,7 @@ $(document).ready(function () {
     // Teste si la cellule est permutable (qu'elle est pleine et voisine de la vide)
     function estPermutable(i,j) {
 
-        if (celluleEstVide(i,j)) {
-            return false;
-
-        } else if ((celluleExiste(i,j-1) && celluleEstVide(i,j-1)) // gauche
+      if ((celluleExiste(i,j-1) && celluleEstVide(i,j-1)) // gauche
             || (celluleExiste(i,j+1) && celluleEstVide(i,j+1))// droite
             || (celluleExiste(i-1,j) && celluleEstVide(i-1,j)) // haut
             || (celluleExiste(i+1,j) && celluleEstVide(i+1,j))) // bas
@@ -96,22 +83,22 @@ $(document).ready(function () {
     // Fonction qui permute une cellule pleine avec une cellule vide
     function permute(i, j) {
         // où est la case vide ?
-        let emptyCase = chercheCaseVide(); // retourne un objet
+        let caseVide = chercheCaseVide(); // retourne un objet
 
         // est-ce permutable ?
         let casePerm = estPermutable(i,j);
 
         //récupère la valeur de la cellule pleine
-        let fullCase = plateau[i][j];
+        let casePleine = plateau[i][j];
 
         //récupére la valeur de la cellule vide
-        let newEmptyCase = plateau[emptyCase.i][emptyCase.j]; // nomVariable.clé car objet
+        let newcaseVide = plateau[caseVide.i][caseVide.j]; // nomVariable.clé car objet
 
         // permuter
         if(casePerm === true){
-            plateau[i][j]= newEmptyCase;
-            plateau[emptyCase.i][emptyCase.j] = fullCase ;
-            draw();
+            plateau[i][j]= newcaseVide;
+            plateau[caseVide.i][caseVide.j] = casePleine ;
+            redessinePlateau();
         }
     };
 });// Fin du document pas touche
